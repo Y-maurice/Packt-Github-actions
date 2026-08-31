9e27859f141110b1486b4122427812ad4d60050e
Youssef Maurice <abc.abcd202020@gmail.com>
2026-08-31 15:28:49 +0300

    feat(17-custom-actions): rename target-branch input to head branch + code refactoring

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -2,19 +2,42 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
 
+const setupGit = async () => {
+    await exec.exec(`git config --global user.name "gh-automation"`);
+    await exec.exec(`git config --global user.emaili "gh-automation@gmail.com"`);
+};
+
 const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
 const validateDirectoryName = ({ dirName }) => /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
 
 
+const setUpLogger = ({debug, prefix} = {debug: false, prefix: ''}) => ({
+
+    debug: (message) => {
+        if(debug){
+            core.info(`DEBUG ${prefix}${prefix?  ' : ' : ''}${message}`);
+
+        }
+    },
+
+    info: (message) => {
+        core.info(`${prefix}${prefix?  ' : ' : ''}${message}`);
+    },
+
+    error: (message) => {
+        core.error(`${prefix}${prefix?  ' : ' : ''}${message}`);
+    }
+});
 
 
 async function run() {
 
     const baseBranch = core.getInput('base-branch', { required: true });
-    const targetBranch = core.getInput('target-branch', { required: true });
+    const headBranch = core.getInput('head-branch', {required : true});
     const ghToken = core.getInput('gh-token', { required: true });
     const workingDir = core.getInput('working-directory', { required: true });
     const debug = core.getBooleanInput('debug');
+    const logger = setUpLogger({debug, prefix: '[js-dependency-update]'});
 
     const commonExecOpts = {
         cwd: workingDir
@@ -22,13 +45,14 @@ async function run() {
 
     core.setSecret(ghToken);
 
+    logger.debug('Validating inputs - base-branch, head-branch, working-directory');
     if (!validateBranchName({ branchName: baseBranch })) {
         core.setFailed('Invalid base-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots,  and forward slashes');
         return;
     }
 
-    if (!validateBranchName({ branchName: targetBranch })) {
-        core.setFailed('Invalid target-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots,  and forward slashes');
+    if (!validateBranchName({ branchName: headBranch })) {
+        core.setFailed('Invalid head-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots,  and forward slashes');
         return;
     }
 
@@ -37,9 +61,11 @@ async function run() {
         return;
     }
 
-    core.info(`[js-dependency-update] : base branch is ${baseBranch}`);
-    core.info(`[js-dependency-update] : target branch is ${targetBranch}`);
-    core.info(`[js-dependency-update] : working directory is ${workingDir}`);
+    logger.debug(`base branch is ${baseBranch}`);
+    logger.debug(`head branch is ${headBranch}`);
+    logger.debug(`working directory is ${workingDir}`);
+
+    logger.debug('Checking for package updates')
 
     await exec.exec('npm update', [], {
         ...commonExecOpts
@@ -50,10 +76,11 @@ async function run() {
     });
 
     if (gitStatus.stdout.length > 0) {
-        core.info('[js-dependency-update] : There are updates available!');
-        await exec.exec(`git config --global user.name "gh-automation"`);
-        await exec.exec(`git config --global user.emaili "gh-automation@gmail.com"`);
-        await exec.exec(`git checkout -b ${targetBranch}`, [], {
+        logger.debug('There are updates available!');
+        logger.debug('Setting up git')
+        await setupGit();
+        logger.debug('Commiting and pushing package.json changes')
+        await exec.exec(`git checkout -b ${headBranch}`, [], {
             ...commonExecOpts
         });
         await exec.exec(`git add package.json package-lock.json`, [], {
@@ -62,41 +89,43 @@ async function run() {
         await exec.exec(`git commit -m "chore: update dependencies"`, [], {
             ...commonExecOpts
         });
-        await exec.exec(`git push -u origin ${targetBranch} --force`, [], {
+        await exec.exec(`git push -u origin ${headBranch} --force`, [], {
             ...commonExecOpts
         });
 
+        logger.debug('Fetching octokit API')
         const oktokit = github.getOctokit(ghToken);
 
         try {
+            logger.debug(`Creating PR using head branch ${headBranch}`)
             await oktokit.rest.pulls.create({
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo,
                 title: 'Update NPM dependencies',
                 body: `This pull request updates NPM packages `,
                 base: baseBranch,
-                head: targetBranch
+                head: headBranch
 
             });
         } catch (e) {
-            core.error('[js-dependency-update] : Something went wrong while creating the PR. check logs below');
+            logger.error('Something went wrong while creating the PR. check logs below');
             core.setFailed(e.message);
-            core.error(e);
+            logger.error(e);
         }
 
     } else {
-        core.info('[js-dependency-update] : No updates at this point in time. ');
+        logger.info('No updates at this point in time. ');
     }
 
     /*
         1. parse inputs:
          1.1 base-branch from which to check for updates
-         1.2 target-branch to use to create the PR
+         1.2 head-branch to use to create the PR
          1.3 Github Token for authentication purposes (to create PRs)
          1.4 Working directory for which to check for dependencies
         2. Execute the npm update command within the working directory
         3. Check whether there are modified package.json files
-            3.1 if there are modified files, create a PR to the base-branch using the target-branch
+            3.1 if there are modified files, create a PR to the base-branch using the head-branch
             3.2 otherwise, conclude the custom action
      */
 }
